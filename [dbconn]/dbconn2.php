57722d279ed07a48063fedee68eb79904d424904
Александр <49386760+hackimov@users.noreply.github.com>
2019-12-05 23:04:48 +0500

    TZPOLICE

--- [dbconn]/dbconn2.php
+++ [dbconn]/dbconn2.php
@@ -1,7 +1,7 @@
 <?
-//$link = mysql_connect('192.168.253.6', 'tzpolice_test', 'pWemi45bnjms') or die ('Could not connect to MySQL');
+//$link = mysql_connect('192.168.253.6', '', '') or die ('Could not connect to MySQL');
 
-$link = mysql_connect('192.168.253.6', 'tzpolice_test', 'oXNrFiUGWCoHEp9Z') or die ('Could not connect to MySQL');
+$link = mysql_connect('192.168.253.6', '', '') or die ('Could not connect to MySQL');
 
 mysql_select_db ('tzpolice_test') or die ('Could not select database');
 
